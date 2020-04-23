3a775f1417ca385c0092651572edd214263f59f0
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-04-24 08:22:59 +0900

    first commit

---
+++ src/test/java/com/cheeringlocalrestaurant/CheeringLocalRestaurantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cheeringlocalrestaurant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheeringLocalRestaurantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
