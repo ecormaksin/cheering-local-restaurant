f89641ef7e0a0cd22a2ecddcd83b0c12c5ee72b4
ecormaksin <truth_jp_4133@yahoo.co.jp>
2020-05-27 14:01:38 +0900

    エンティティをjavax.validationを使ってバリデーションする

--- src/main/java/com/cheeringlocalrestaurant/domain/model/restaurant/RestaurantName.java
+++ src/main/java/com/cheeringlocalrestaurant/domain/model/restaurant/RestaurantName.java
@@ -1,39 +1,31 @@
 package com.cheeringlocalrestaurant.domain.model.restaurant;
 
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-
-import com.cheeringlocalrestaurant.presentation.controller.restaurant.RestaurantTempRegisterForm;
+import com.cheeringlocalrestaurant.domain.model.ValidationConcern;
 import com.cheeringlocalrestaurant.validation.notallblank.NotAllBlank;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Value;
 
 /**
  * 飲食店名
  */
 @Getter
-@Value
-public class RestaurantName {
+@EqualsAndHashCode(callSuper=false)
+public class RestaurantName extends ValidationConcern {
 
     /** 最小文字数 */
     public static final int MIN_SIZE = 1;
     /** 最大文字数 */
     public static final int MAX_SIZE = 50;
     
+	private static final long serialVersionUID = 1L;
+
 	@NotAllBlank(max = MAX_SIZE)
     private final String value;
     
     public RestaurantName(final String value) {
     	this.value = value;
 
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		Validator validator = factory.getValidator();
-		Set<ConstraintViolation<RestaurantName>> violations = validator.validate(this);
-		if (violations.size() != 0 ) throw new IllegalArgumentException();
+		this.validate(this);
     }
 }
--- src/test/java/com/cheeringlocalrestaurant/domain/model/restaurant/RestaurantNameTest.java
+++ src/test/java/com/cheeringlocalrestaurant/domain/model/restaurant/RestaurantNameTest.java
@@ -2,13 +2,47 @@ package com.cheeringlocalrestaurant.domain.model.restaurant;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
 class RestaurantNameTest {
+
+	@Test
+	void _正常な場合は例外が発生しない() {
+		try {
+			new RestaurantName("いろは食堂");
+		} catch (Exception e) {
+			fail();
+		}
+	}
 
 	@Test
 	void _nullの場合は例外が発生する() {
 		assertThrows(IllegalArgumentException.class, () -> {new RestaurantName(null);});
 	}
 
+	@Test
+	void _長さ0の場合は例外が発生する() {
+		assertThrows(IllegalArgumentException.class, () -> {new RestaurantName("");});
+	}
+
+	@Test
+	void _半角スペースのみの場合は例外が発生する() {
+		assertThrows(IllegalArgumentException.class, () -> {new RestaurantName(" ");});
+	}
+
+	@Test
+	void _全角スペースのみの場合は例外が発生する() {
+		assertThrows(IllegalArgumentException.class, () -> {new RestaurantName("　");});
+	}
+
+	@Test
+	void _タブのみの場合は例外が発生する() {
+		assertThrows(IllegalArgumentException.class, () -> {new RestaurantName("\t");});
+	}
+	
+	@Test
+	void _50文字を超える場合は例外が発生する() {
+		assertThrows(IllegalArgumentException.class, () -> {new RestaurantName(StringUtils.repeat("あ", 51));});
+	}
 }
