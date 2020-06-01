97871240cf1a5fa5f90659f97f068535c2982856
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-06-02 08:21:55 +0900

    仕掛中
    TestEntityManagerを使って永続化しようとすると
    `PersistentObjectException: detached entity passed to persist`
    が発生する。

---
+++ src/test/java/com/cheeringlocalrestaurant/infra/db/repository/QueryDslTest.java
@@ -0,0 +1,37 @@
+package com.cheeringlocalrestaurant.infra.db.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import com.cheeringlocalrestaurant.infra.db.jpa.EntityUtil;
+import com.cheeringlocalrestaurant.infra.db.jpa.entity.Resto;
+import com.cheeringlocalrestaurant.infra.db.jpa.repository.RestroRepository;
+
+@DataJpaTest
+class QueryDslTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+    
+    @Autowired
+    private RestroRepository restroRepository;
+
+    @Test
+    void test() {
+	Resto restoExp = new Resto();
+	restoExp.setRestaurantId(1L);
+	EntityUtil.setCommonColumn(restoExp, "127.0.0.1");
+	restroRepository.save(restoExp);
+//	entityManager.persist(restoExp);
+	
+	Resto restoAct = restroRepository.getOne(1L);
+	assertNotNull(restoAct);
+
+//	entityManager.flush();
+    }
+
+}
