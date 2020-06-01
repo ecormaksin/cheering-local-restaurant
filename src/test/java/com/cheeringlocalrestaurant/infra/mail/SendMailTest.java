b0ce1d5de2073f117d6d95dab390a7d122e8ab25
ecormaksin <truth_jp_4133@yahoo.co.jp>
2020-06-01 13:06:54 +0900

    仕掛中
    GreenMailの動作確認、FreeMarkerのテンプレート導入

--- src/test/java/com/cheeringlocalrestaurant/infra/mail/SendMailTest.java
+++ src/test/java/com/cheeringlocalrestaurant/infra/mail/SendMailTest.java
@@ -2,7 +2,12 @@ package com.cheeringlocalrestaurant.infra.mail;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +17,14 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import com.cheeringlocalrestaurant.CheeringLocalRestaurantApplication;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import lombok.Data;
 
 @SpringBootTest(classes = CheeringLocalRestaurantApplication.class,
 	webEnvironment = WebEnvironment.NONE)
@@ -21,6 +32,9 @@ class SendMailTest {
 
 	@Autowired
 	private JavaMailSender emailSender;
+	
+	@Autowired
+    private Configuration freemarkerConfig;
 	
 	@Value("${mail.from.address}")
 	private String mailAddressFrom;
@@ -35,6 +49,34 @@ class SendMailTest {
 		message.setTo(mailAddressTo); 
 		message.setSubject("送信テスト" + LocalDateTime.now().toString()); 
 		message.setText("Test");
+		emailSender.send(message);
+	}
+	
+	@Data
+	class TestModel {
+		private String subject = "ログインURLのお知らせ";
+		private String toAddress;
+		private String token = UUID.randomUUID().toString();
+	}
+	
+	@Test
+	void sendByFreeMarkerTemplate() throws Exception {
+		MimeMessage message = emailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+		
+		freemarkerConfig.setClassForTemplateLoading(this.getClass(), "/mail_templates");
+		
+		TestModel testModel = new TestModel();
+		testModel.setToAddress(mailAddressTo);
+
+		Template t = freemarkerConfig.getTemplate("notify_login_url.ftl");
+		String text = FreeMarkerTemplateUtils.processTemplateIntoString(t, null);
+		
+		helper.setFrom(mailAddressFrom);
+		helper.setTo(testModel.getToAddress());
+		helper.setText(text, true);
+		helper.setSubject("");
+		
 		emailSender.send(message);
 	}
 
