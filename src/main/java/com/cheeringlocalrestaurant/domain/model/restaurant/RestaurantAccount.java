bd48cc1b8a9e104b31602bf397ef2832b4efcf2a
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-06-03 00:01:32 +0900

    QueryDslによるジョインの動作確認

--- src/main/java/com/cheeringlocalrestaurant/domain/model/restaurant/RestaurantAccount.java
+++ src/main/java/com/cheeringlocalrestaurant/domain/model/restaurant/RestaurantAccount.java
@@ -7,10 +7,12 @@ import com.cheeringlocalrestaurant.domain.type.UserId;
 import com.cheeringlocalrestaurant.domain.type.restaurant.RestaurantId;
 import com.cheeringlocalrestaurant.domain.type.restaurant.RestaurantName;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 
+@AllArgsConstructor
 @Builder
 @Getter
 public class RestaurantAccount {
@@ -27,4 +29,21 @@ public class RestaurantAccount {
     @NonNull
     @Valid
     private MailAddress mailAddress;
+    
+    public RestaurantAccount(Long userId, Long restaurantId, String restaurantName, String mailAddress) {
+        this.userId = new UserId(userId);
+        this.restaurantId = new RestaurantId(restaurantId);
+        this.restaurantName = new RestaurantName(restaurantName);
+        this.mailAddress = new MailAddress(mailAddress);
+    }
+    
+    public String toString() {
+        // @formatter:off
+        return String.format("userId: %s, restaurantId: %s, restaurantName: %s, mailAddress: %s", 
+                userId.getValue().toString(),
+                restaurantId.getValue().toString(),
+                restaurantName.getValue()
+                ,mailAddress.getValue());
+        // @formatter:on
+    }
 }
--- src/test/java/com/cheeringlocalrestaurant/infra/db/repository/RestaurantRepositoryTest.java
+++ src/test/java/com/cheeringlocalrestaurant/infra/db/repository/RestaurantRepositoryTest.java
@@ -2,12 +2,12 @@ package com.cheeringlocalrestaurant.infra.db.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import javax.persistence.EntityManagerFactory;
-import javax.transaction.Transactional;
-import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
@@ -16,39 +16,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-import com.cheeringlocalrestaurant.CheeringLocalRestaurantApplication;
+import com.cheeringlocalrestaurant.domain.model.restaurant.RestaurantAccount;
 import com.cheeringlocalrestaurant.domain.model.restaurant.RestaurantRepository;
 import com.cheeringlocalrestaurant.domain.model.restaurant.RestaurantTempRegister;
-import com.cheeringlocalrestaurant.domain.type.MailAddress;
-import com.cheeringlocalrestaurant.domain.type.UserId;
+import com.cheeringlocalrestaurant.domain.type.UserRole;
 import com.cheeringlocalrestaurant.domain.type.restaurant.RestaurantId;
-import com.cheeringlocalrestaurant.domain.type.restaurant.RestaurantName;
 import com.cheeringlocalrestaurant.infra.db.jpa.entity.QResto;
 import com.cheeringlocalrestaurant.infra.db.jpa.entity.QRestoAccount;
 import com.cheeringlocalrestaurant.infra.db.jpa.entity.QRestoHistory;
 import com.cheeringlocalrestaurant.infra.db.jpa.entity.QRestoName;
 import com.cheeringlocalrestaurant.infra.db.jpa.entity.QUser;
 import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.querydsl.jpa.sql.JPASQLQuery;
 import com.querydsl.sql.H2Templates;
 import com.querydsl.sql.SQLTemplates;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @AutoConfigureTestEntityManager
 @Transactional
+@Slf4j
 class RestaurantRepositoryTest {
 
     @Autowired
     private TestEntityManager entityManager;
-    
-    private JPAQueryFactory queryFactory;
     
     @Autowired
     private SQLTemplates templates;
@@ -62,23 +55,6 @@ class RestaurantRepositoryTest {
         SQLTemplates templates() {
             return new H2Templates();
         }
-    }
-
-    @BeforeEach
-    void setup() {
-        queryFactory = new JPAQueryFactory(entityManager.getEntityManager());
-    }
-
-    @AllArgsConstructor
-    @Getter
-    class RestaurantAccountDTO {
-
-        private Long userId;
-        private Long restaurantId;
-        private String restaurantName;
-        private String mailAddress;
-        
-        protected RestaurantAccountDTO() {}
     }
 
     @Test
@@ -97,12 +73,32 @@ class RestaurantRepositoryTest {
         QRestoHistory qRestoHistory = QRestoHistory.restoHistory;
         QRestoName qRestoName = QRestoName.restoName;
         JPASQLQuery<?> query = new JPASQLQuery<Void>(entityManager.getEntityManager(), templates);
-        RestaurantAccountDTO restaurantAccountDTO 
-                = query.select(Projections.constructor(RestaurantAccountDTO.class, 
+        Date systemDate = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        // @formatter:off
+        RestaurantAccount restaurantAccount 
+                = query.select(Projections.constructor(RestaurantAccount.class, 
                         qUser.userId, qResto.restaurantId, qRestoName.restaurantName, qUser.mailAddress))
                         .from(qUser)
                             .innerJoin(qRestoAccount)
-                        .fetchOne();
+                                .on(qUser.userId.eq(qRestoAccount.userId))
+                            .innerJoin(qResto)
+                                .on(qRestoAccount.restaurantId.eq(qResto.restaurantId))
+                            .innerJoin(qRestoHistory)
+                                .on(qResto.restaurantId.eq(qRestoHistory.restaurantId))
+                            .innerJoin(qRestoName)
+                                .on(qRestoHistory.restaurantHistoryId.eq(qRestoName.restaurantHistoryId))
+                        .where(qUser.mailAddress.eq(email)
+                                .and(qUser.userRole.eq(UserRole.RESTAURANT_ADMIN.getValue()))
+                                .and(qRestoHistory.startDate.loe(systemDate))
+                                .and(qRestoHistory.endDate.goe(systemDate))
+                        ).fetchOne();
+        // @formatter:on
+        assertNotNull(restaurantAccount);
+        assertNotNull(restaurantAccount.getUserId());
+        assertNotNull(restaurantAccount.getRestaurantId());
+        assertEquals(name, restaurantAccount.getRestaurantName().getValue());
+        assertEquals(email, restaurantAccount.getMailAddress().getValue());
+        log.info(restaurantAccount.toString());
     }
 
 }
