b999422195b98a8f5a7244a9fb2ad3f0562ccbd5
ecormaksin <truth_jp_4133@yahoo.co.jp>
2020-06-02 11:52:43 +0900

    1つのテーブルでQueryDslが動作することを確認

--- src/test/java/com/cheeringlocalrestaurant/infra/db/repository/QueryDslTest.java
+++ src/test/java/com/cheeringlocalrestaurant/infra/db/repository/QueryDslTest.java
@@ -2,36 +2,63 @@ package com.cheeringlocalrestaurant.infra.db.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import com.cheeringlocalrestaurant.domain.type.UserRole;
 import com.cheeringlocalrestaurant.infra.db.jpa.EntityUtil;
+import com.cheeringlocalrestaurant.infra.db.jpa.entity.QUser;
 import com.cheeringlocalrestaurant.infra.db.jpa.entity.Resto;
+import com.cheeringlocalrestaurant.infra.db.jpa.entity.User;
 import com.cheeringlocalrestaurant.infra.db.jpa.repository.RestroRepository;
+import com.cheeringlocalrestaurant.infra.db.jpa.repository.UserRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @DataJpaTest
 class QueryDslTest {
 
     @Autowired
     private TestEntityManager entityManager;
-
+    
+    private JPAQueryFactory queryFactory;
+    
     @Autowired
     private RestroRepository restroRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+    
+    @BeforeEach
+    void setup() {
+        queryFactory = new JPAQueryFactory(entityManager.getEntityManager());
+    }
+
     @Test
-    void test() {
-        Resto restoExp = new Resto();
-        restoExp.setRestaurantId(1L);
-        EntityUtil.setCommonColumn(restoExp, "127.0.0.1");
-        restroRepository.save(restoExp);
-//	entityManager.persist(restoExp);
+    void _1つのテーブルでの簡単なテスト() {
+        
+    	final String mailAddress = "iroha@example.com";
 
-        Resto restoAct = restroRepository.getOne(1L);
-        assertNotNull(restoAct);
+        QUser qUser = QUser.user;
+        
+        User user = new User();
+        user.setMailAddress(mailAddress);
+        user.setUserRole(UserRole.RESTAURANT_ADMIN.getValue());
+        EntityUtil.setCommonColumn(user, "127.0.0.1");
+        user = userRepository.save(user);
 
-//	entityManager.flush();
+        assertNotNull(user.getUserId());
+        
+        User searched = queryFactory.selectFrom(qUser)
+            .where(qUser.mailAddress.eq(mailAddress)
+                    .and(qUser.userRole.eq(UserRole.RESTAURANT_ADMIN.getValue())))
+            .fetchOne();
+        
+        assertNotNull(searched);
+        assertEquals(user.getUserId(), searched.getUserId());
+        assertEquals(mailAddress, searched.getMailAddress());
     }
 
 }
