b65411d50105da1cd9e2d03eef88e6b7b307aa29
ecormaksin <truth_jp_4133@yahoo.co.jp>
2020-06-05 17:48:22 +0900

    仕掛中
    飲食店仮登録におけるデータベースの詳細を確認

--- src/main/java/com/cheeringlocalrestaurant/infra/db/jpa/util/EntityUtil.java
+++ src/main/java/com/cheeringlocalrestaurant/infra/db/jpa/util/EntityUtil.java
@@ -2,15 +2,40 @@ package com.cheeringlocalrestaurant.infra.db.jpa.util;
 
 import java.lang.reflect.Field;
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 import com.cheeringlocalrestaurant.domain.type.RemoteIpAddress;
+import com.cheeringlocalrestaurant.infra.db.JavaTimeDateConverter;
 
 final class EntityUtil {
 
     public static <E> void setCommonColumn(E entity, RemoteIpAddress remoteIpAddress) {
+        setStartDate(entity);
+        setEndDate(entity);
         setRegisteredTimestamp(entity);
         setRemoteIpAddress(entity, remoteIpAddress);
+    }
+
+    private static <E> void setStartDate(E entity) {
+        setDate(entity, "startDate", LocalDate.of(1900, 1, 1));
+    }
+
+    private static <E> void setEndDate(E entity) {
+        setDate(entity, "endDate", LocalDate.of(9999, 12, 31));
+    }
+
+    private static <E> void setDate(E entity, String fieldName, LocalDate defaultLocalDate) {
+        try {
+            Field field = getField(entity, fieldName);
+            Date date = (Date) field.get(entity);
+            if (null != date) return;
+            date = JavaTimeDateConverter.toDateFromLocalDate(defaultLocalDate);
+            field.set(entity, date);
+        } catch (Exception e) {
+
+        }
     }
 
     private static <E> void setRegisteredTimestamp(E entity) {
--- src/test/java/com/cheeringlocalrestaurant/infra/db/repository/RestaurantRepositoryTest.java
+++ src/test/java/com/cheeringlocalrestaurant/infra/db/repository/RestaurantRepositoryTest.java
@@ -4,10 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cheeringlocalrestaurant.TestDataUtils;
 import com.cheeringlocalrestaurant.domain.model.restaurant.RestaurantAccount;
@@ -17,20 +21,44 @@ import com.cheeringlocalrestaurant.domain.type.MailAddress;
 import com.cheeringlocalrestaurant.domain.type.RemoteIpAddress;
 import com.cheeringlocalrestaurant.domain.type.account.UserId;
 import com.cheeringlocalrestaurant.domain.type.restaurant.RestaurantId;
+import com.cheeringlocalrestaurant.infra.db.jpa.entity.QRestoHistory;
+import com.cheeringlocalrestaurant.infra.db.jpa.entity.Resto;
+import com.cheeringlocalrestaurant.infra.db.jpa.entity.RestoHistory;
+import com.cheeringlocalrestaurant.infra.db.jpa.entity.RestoName;
+import com.cheeringlocalrestaurant.infra.db.jpa.repository.RestoNameRepository;
+import com.cheeringlocalrestaurant.infra.db.jpa.repository.RestroRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @Slf4j
+@Transactional
+@AutoConfigureTestEntityManager
 class RestaurantRepositoryTest {
 
     @Autowired
     private RestaurantRepository restaurantRepository;
     
+    @Autowired
+    TestEntityManager entityManager;
+    @Autowired
+    private RestroRepository restroRepository;
+    @Autowired
+    private RestoNameRepository restoNameRepository;
+    
+    private JPAQueryFactory queryFactory;
+    
+    private static final String restoNameStr = TestDataUtils.RESTAURANT_NAME;
     private static final String mailAddressStr = TestDataUtils.MAIL_ADDRESS;
     private static final RemoteIpAddress remoteIpAddress = TestDataUtils.remoteIpAddress;
     private static final RestaurantTempRegister restaurantTempRegister = TestDataUtils.restaurantTempRegister;
     
+    @BeforeEach
+    void setup() {
+        queryFactory = new JPAQueryFactory(entityManager.getEntityManager());
+    }
+
     @Test
     void _仮登録() throws Exception {
 
@@ -62,6 +90,35 @@ class RestaurantRepositoryTest {
             assertNotNull(mailAddrAct);
             assertEquals(mailAddressStr, mailAddrAct);
             log.info(restaurantAccount.toString());
+            
+
+            // ** エンティティの確認
+            // 飲食店
+            Resto resto = restroRepository.getOne(restoIdLng);
+            assertNotNull(resto.getRegisteredTimestamp());
+            assertNotNull(resto.getRemoteIpAddress());
+            // 飲食店履歴
+            QRestoHistory qRestoHistory = QRestoHistory.restoHistory;
+            // @formatter:off
+            RestoHistory restoHistory = queryFactory.selectFrom(qRestoHistory)
+                    .where(qRestoHistory.restaurantId.eq(restoIdLng))
+                    .fetchOne();
+            // @formatter:on
+            assertNotNull(restoHistory);
+            Long restaurantHistoryId = restoHistory.getRestaurantHistoryId();
+            assertNotNull(restaurantHistoryId);
+            assertNotNull(restoHistory.getEndDate());
+            assertNotNull(restoHistory.getRegisteredTimestamp());
+            assertNotNull(restoHistory.getRemoteIpAddress());
+            Long restoHistRestoId = restoHistory.getRestaurantId();
+            assertNotNull(restoHistRestoId);
+            assertEquals(restoIdLng, restoHistRestoId);
+            assertNotNull(restoHistory.getStartDate());
+            // 飲食店名
+            RestoName restoName = restoNameRepository.getOne(restaurantHistoryId);
+            assertNotNull(restoName);
+            String restoNameStrAct = restoName.getRestaurantName();
+            assertEquals(restoNameStr, restoNameStrAct);
 
         } catch (Exception e) {
             fail(e);
